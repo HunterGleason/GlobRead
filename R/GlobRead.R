3f2332c0e1d404aba83ac63cf7a78af27b4383a6
Hunter Gleason <Hunter.Gleason@gov.bc.ca>
2020-12-18 10:56:25 -0800

    Added ERAI as reanalysis option, added time stamp to scaled nc file name, and improved documentation

--- R/GlobRead.R
+++ R/GlobRead.R
@@ -7,21 +7,27 @@
 #'The user must specify which GlobSim reanalysis, variable, and station to obtain the time series for.
 #'Finally, the path to the scaled GlobSim output nc files must be provided.
 #'
-#' @param dataset One of ERA5, JRA55 or MERRA2 specifying which reanalysis to use.
-#' @param var_name Which GlobSim climate variable to return, see 'get_var_names'
-#' @param station_name Which station 'site' to obtain time series data for, see 'station_names'
+#' @param dataset String. One of ERA5, ERAI, JRA55 or MERRA2 specifying which reanalysis to use.
+#' @param var_name String. Which GlobSim climate variable to return, see 'get_var_names'
+#' @param station_name String. Which station 'site' to obtain time series data for, see 'station_names'
+#' @param scaled_path String. Path to the directory with the `scaled` output from GlobSim
+#' @param time_step Float. The time step (in hours) used for interpolation and scaling of GlobSim time series.
 #' @return A data frame with one column representing the time stamp of the observation, and the other the scaled value.
 #' @export
-get_glob_TS<-function(dataset,var_name,station_name,scaled_path)
+get_glob_TS<-function(dataset,var_name,station_name,scaled_path,time_step)
 {
 
+  time_step<-sprintf("%.1f",time_step)
+
   if(dataset == 'ERA5'){
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_era5_1.0h.nc",sep=""))
-  }else if(dataset == 'JRA55'){
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_jra55_1.0h.nc",sep=""))
-  }else{
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_merra2_1.0h.nc",sep=""))
-  }
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_era5_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'ERAI'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_erai_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'JRA56'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_jra55_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'MARRA2'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_merra2_',time_step,'h.nc',sep=''))
+  }else{cat('No dataset with that name ...')}
 
   stations<-ncdf4::ncvar_get(reanalysis,"station_name")
 
@@ -35,11 +41,13 @@ get_glob_TS<-function(dataset,var_name,station_name,scaled_path)
 
   if(dataset=='ERA5'){
     ts$DateTime<-lubridate::as_datetime(c(ts$DateTime),origin="1900-01-01")
+  }else if(dataset=='ERAI'){
+    ts$DateTime<-lubridate::as_datetime(c(ts$DateTime),origin="1900-01-01")
   }else if(dataset=='JRA55'){
     ts$DateTime<-lubridate::as_datetime(c(ts$DateTime*60*60),origin="1800-01-01")
-  }else{
+  }else if(dataset == 'MERRA2'){
     ts$DateTime<-lubridate::as_datetime(c(ts$DateTime),origin="1980-01-01")
-  }
+  }else{cat('No dataset with that name ...')}
 
   return(ts)
 
@@ -49,18 +57,25 @@ get_glob_TS<-function(dataset,var_name,station_name,scaled_path)
 #'
 #'This function extracts the climate variable names present within a scaled GlobSim nc dataset.
 #'
-#' @param dataset One of ERA5, JRA55 or MERRA2 specifying which reanalysis to use.
+#' @param dataset String. One of ERA5, JRA55 or MERRA2 specifying which reanalysis to use.
+#' @param scaled_path String. Path to the directory with the `scaled` output from GlobSim.
+#' @param time_step Float. The time step (in hours) used for interpolation and scaling of GlobSim time series.
 #' @return A vector of GlobSim climate varible names specific to a given reanalysis dataset.
 #' @export
-get_var_names<-function(dataset,scaled_path)
+get_var_names<-function(dataset,scaled_path,time_step)
 {
+
+  time_step<-sprintf("%.1f",time_step)
+
   if(dataset == 'ERA5'){
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_era5_1.0h.nc",sep=""))
-  }else if(dataset == 'JRA55'){
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_jra55_1.0h.nc",sep=""))
-  }else{
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_merra2_1.0h.nc",sep=""))
-  }
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_era5_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'ERAI'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_erai_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'JRA56'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_jra55_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'MARRA2'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_merra2_',time_step,'h.nc',sep=''))
+  }else{cat('No dataset with that name ...')}
 
   return(names(reanalysis$var))
 }
@@ -70,17 +85,24 @@ get_var_names<-function(dataset,scaled_path)
 #'This function extracts the station 'site' names present within a scaled GlobSim nc dataset.
 #'
 #' @param dataset One of ERA5, JRA55 or MERRA2 specifying which reanalysis to use, in this case results should be the same.
+#' @param scaled_path String. Path to the directory with the `scaled` output from GlobSim.
+#' @param time_step Float. The time step (in hours) used for interpolation and scaling of GlobSim time series.
 #' @return A vector of GlobSim station 'site' names specific to a given reanalysis dataset.
 #' @export
-station_names<-function(dataset,scaled_path)
+station_names<-function(dataset,scaled_path,time_step)
 {
+
+  time_step<-sprintf("%.1f",time_step)
+
   if(dataset == 'ERA5'){
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_era5_1.0h.nc",sep=""))
-  }else if(dataset == 'JRA55'){
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_jra55_1.0h.nc",sep=""))
-  }else{
-    reanalysis <-ncdf4::nc_open(paste(scaled_path,"scaled_merra2_1.0h.nc",sep=""))
-  }
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_era5_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'ERAI'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_erai_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'JRA56'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_jra55_',time_step,'h.nc',sep=''))
+  }else if(dataset == 'MARRA2'){
+    reanalysis <-ncdf4::nc_open(paste(scaled_path,'scaled_merra2_',time_step,'h.nc',sep=''))
+  }else{cat('No dataset with that name ...')}
 
 
   stations<-ncdf4::ncvar_get(reanalysis,"station_name")
@@ -89,24 +111,4 @@ station_names<-function(dataset,scaled_path)
 
 }
 
-####Example of use, un-comment####
 
-# #Example of 'get_glob_TS' function use, must provide reanalysis short name, desired reanalysis variable and desired station name.
-#
-# #E.g., List stations, should be same for all datasets
-# station_names('ERA5')
-# station_names('JRA55')
-# station_names('MERRA2')
-#
-# #E.g., List variable names for ERA5
-# get_var_names('ERA5')
-#
-#
-# #Get 2-m air temp for each reanalysis at the 'atl_sch' station location, plot each time series
-# era5_temp<-get_glob_TS('ERA5','AIRT_ERA5_C_sur','atl_sch')
-# jra55_temp<-get_glob_TS('JRA55','AIRT_JRA55_C_sur','atl_sch')
-# merra2_temp<-get_glob_TS('MERRA2','AIRT_MERRA2_C_sur','atl_sch')
-#
-# plot(era5_temp$DateTime,era5_temp$AIRT_ERA5_C_sur,type='l')
-# lines(jra55_temp$DateTime,jra55_temp$AIRT_JRA55_C_sur,col='blue')
-# lines(merra2_temp$DateTime,merra2_temp$AIRT_MERRA2_C_sur,col='red')
---
+++ README.md
@@ -0,0 +1,15 @@
+### Overview
+
+The **GlobRead** package is a simple package for extracting data within the scaled output files of [GlobSim](https://gmd.copernicus.org/articles/12/4661/2019/) using the [**netcdf4**](https://cran.r-project.org/web/packages/ncdf4/ncdf4.pdf) package. This includes site/station names, reanalysis variable names, and the time series data present in the scaled GlobSim output. 
+
+### Installation
+
+You can install the **GlobRead** package from GitHub via the [**remotes** package](https://remotes.r-lib.org):
+
+```r
+remotes::install_github('HunterGleason/GlobRead')
+```
+
+### Usage
+
+NA 
